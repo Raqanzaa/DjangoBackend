e1bc9383591fe9bba7faab9c7a424a46ba1b5da0
Raqanzaa <ahmadrozaq34@gmail.com>
2025-09-07 07:59:07 +0700

    fix: google oauth profile photo

--- apps/accounts/pipeline.py
+++ apps/accounts/pipeline.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import PermissionDenied
 from apps.accounts.models import CustomUser
 
+
 def ensure_user_exists(backend, uid, user=None, *args, **kwargs):
     """
     Ensure only existing users can login with Google.
@@ -8,7 +9,7 @@ def ensure_user_exists(backend, uid, user=None, *args, **kwargs):
     """
     if user:  # already matched to a social account
         return {"user": user}
-    
+
     email = kwargs.get("details", {}).get("email")
     if not email:
         raise PermissionDenied("No email provided by OAuth provider.")
@@ -20,11 +21,19 @@ def ensure_user_exists(backend, uid, user=None, *args, **kwargs):
         raise PermissionDenied("This account is not registered. Please sign up first.")
 
 def save_profile_picture(backend, user, response, *args, **kwargs):
+    """
+    Social auth pipeline function to save a user's profile picture.
+    This version has a more robust check for different placeholder images.
+    """
     picture_url = None
 
     if backend.name == "google-oauth2":
         picture_url = response.get("picture")
 
     if picture_url:
-        user.profile_picture = picture_url
+        if 'googleusercontent.com/profile/picture' in picture_url or picture_url.endswith('/photo.jpg'):
+            user.profile_picture = None
+        else:
+            user.profile_picture = picture_url
+        
         user.save()
